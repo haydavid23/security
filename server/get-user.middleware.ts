5e882c9b9bc5b4ec1672e4ae18152ee17a5b9550
David Hay <david@Davids-MacBook-Pro.local>
2022-02-22 09:52:35 -0500

    d

---
+++ server/get-user.middleware.ts
@@ -0,0 +1,33 @@
+import {Request, Response, NextFunction} from "express"
+import { decodeJwt } from "./security.utils";
+
+
+export function retrieveUserIdFromRequest(req:Request, res:Response, next:NextFunction)
+{
+    const jwt = req.cookies["SESSIONID"];
+
+    if(jwt)
+    {
+        handleSessionCookie(jwt, req).then(()=>{
+            next()
+        }).catch(()=>{
+            next()
+        })
+        
+    }
+      
+} 
+
+async function handleSessionCookie(jwt:string, req:Request)
+{
+    try{
+        const payload = await decodeJwt(jwt)
+
+        req["userId"] =  payload.subject
+    }
+    catch(error){
+        console.log("Error:Could not extract user from request", error.message)
+    }
+
+
+}
--- server/security.utils.ts
+++ server/security.utils.ts
@@ -6,12 +6,13 @@ const util = require('util');
 const crypto = require('crypto');
 import * as jwt from 'jsonwebtoken';
 import * as fs from "fs";
+import { json } from "body-parser";
 
 
 
 export const randomBytes = util.promisify(crypto.randomBytes);
 
-
+export const signJwt = util.promisify(jwt.sign)
 
 const RSA_PRIVATE_KEY = fs.readFileSync('./demos/private.key');
 
@@ -20,3 +21,18 @@ const RSA_PUBLIC_KEY = fs.readFileSync('./demos/public.key');
 const SESSION_DURATION = 240;
 
 
+export async function createSessionToken(userId:string){
+return  signJwt({}, RSA_PRIVATE_KEY,{
+        algorithm:"RS256",
+        expiresIn:240,
+        subject:userId
+})
+}
+
+
+export async function decodeJwt(token:string)
+{
+    const payload = await jwt.verify(token, RSA_PUBLIC_KEY)
+    console.log(payload)
+    return payload;
+}
